cd65d9f7eaa6fa01a57244d3b08221320a30c308
maxdonetsk <max.popovitch@gmail.com>
2020-10-28 15:31:15 +0200

    hotkey added to show iframe

--- src/background.js
+++ src/background.js
@@ -7,3 +7,21 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     action: 'toggle-show-iframe',
   });
 });
+
+chrome.commands.onCommand.addListener(function (command) {
+  switch (command) {
+    case 'show-iframe':
+      chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        const id = tabs && tabs[0] && tabs[0].id;
+        if (id) {
+          chrome.tabs.sendMessage(id, {
+            action: command,
+          });
+        }
+      });
+      break;
+
+    default:
+      break;
+  }
+});
