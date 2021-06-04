fea392f67c4efb5805d69676e8ddb3c0c47ef8d9
Eagle19243 <eagle19243@gmail.com>
2021-06-04 02:03:06 -0400

    code clean
    - remove select timeframe component
    - remove widgets

--- src/app/pages/dashboard/select-timeframes/timeframe-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TimeframeFilterPipe } from './timeframe-filter.pipe';
-
-describe('TimeframeFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TimeframeFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
